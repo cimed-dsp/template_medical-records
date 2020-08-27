30d465b362720cbee0e55064781dbe85483e38ad
Matt Berry <mjberry@illinois.edu>
2020-08-27 19:59:08 +0000

    reset author

--- setup/install-conda.sh
+++ setup/install-conda.sh
@@ -1,5 +1,3 @@
-bash resize.sh 40
-
 sudo apt-get update --fix-missing
 sudo apt-get install -y libgl1-mesa-glx libegl1-mesa \
   libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
@@ -7,6 +5,6 @@ sudo apt-get install -y libgl1-mesa-glx libegl1-mesa \
 wget --quiet https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh -O anaconda.sh
 bash anaconda.sh -b
 rm anaconda.sh
-source ~/anaconda3/bin/activate
+. ~/anaconda3/bin/activate
 conda init
-source ~/.bashrc
+. ~/.bashrc
